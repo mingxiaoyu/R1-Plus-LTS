5a88ce7d5d4996a99013fff4e168102dea131cb0
mingxiaoyu <fengying0347@163.com>
2022-03-24 16:53:13 +0800

    im

--- immortalwrt.sh
+++ immortalwrt.sh
@@ -2,3 +2,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+svn co https://github.com/mingxiaoyu/luci-app-cloudflarespeedtest/trunk/applications/luci-app-cloudflarespeedtest package/luci-app-cloudflarespeedtest
